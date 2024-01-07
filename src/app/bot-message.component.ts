1acb313ad3aa2244ff2d2aebf897629ba9047b80
Lukas Schmyrczyk <lukas.schmyrczyk@lionmint.com>
2024-01-07 13:53:16 +0100

    Make it possible to mark text while it streams answers

--- src/app/bot-message.component.ts
+++ src/app/bot-message.component.ts
@@ -1,32 +1,48 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+  OnChanges,
+  OnInit,
+} from "@angular/core";
 
 @Component({
   selector: "app-bot-message",
   template: `
-    <div
-      class="flex items-end mb-4 space-x-2">
+    <div class="flex items-end mb-4 space-x-2">
       <div
-        class="flex items-center flex-shrink-0 px-4 py-2 text-white bg-primary-600 dark:bg-primary-500 rounded-lg max-w-[90%]"
+        class=" px-4 py-2 text-white bg-primary-600 dark:bg-primary-500 rounded-lg max-w-[90%]"
       >
-        <p class="text-sm whitespace-pre-wrap">
-          {{ message }}
-          <ng-container *ngIf="links !== undefined">
-            <ng-container *ngFor="let link of links">
-              <br /><br /><strong>{{ link.title }}</strong
-              ><br />
-              <a
-                class="cursor-pointer group inline-flex items-center gap-4 text-primary-200 hover:text-primary-100 transition-colors duration-300"
-                (click)="openViewer.emit(link.url)"
-              >
-                <!--span class="font-sans font-medium text-sm">View here</span-->
-                <i
-                  class="iconify w-4 h-4 group-hover:translate-x-1 transition-transform duration-300"
-                  data-icon="lucide:arrow-right"
-                ></i>
-              </a>
-            </ng-container>
+        <ng-container
+          *ngFor="
+            let line of messageCopy.split(
+              '
+'
+            )
+          "
+        >
+          <p class="text-sm whitespace-pre-wrap">
+            {{ line }}
+          </p>
+        </ng-container>
+        <ng-container *ngIf="links !== undefined">
+          <ng-container *ngFor="let link of links">
+            <br /><br /><strong>{{ link.title }}</strong
+            ><br />
+            <a
+              class="cursor-pointer group inline-flex items-center gap-4 text-primary-200 hover:text-primary-100 transition-colors duration-300"
+              (click)="openViewer.emit(link.url)"
+            >
+              <!--span class="font-sans font-medium text-sm">View here</span-->
+              <i
+                class="iconify w-4 h-4 group-hover:translate-x-1 transition-transform duration-300"
+                data-icon="lucide:arrow-right"
+              ></i>
+            </a>
           </ng-container>
-        </p>
+        </ng-container>
       </div>
     </div>
   `,
@@ -38,14 +54,26 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     `,
   ],
 })
-export class BotMessageComponent {
+export class BotMessageComponent implements OnInit, OnChanges {
   @Input() message: string;
+  messageCopy: string;
   @Input() layout: "chat-bubbles" | "centered" = "chat-bubbles";
   @Input() links?: Array<{ title: string; url: string }>;
   @Output() openViewer = new EventEmitter<string>();
 
   constructor() {
     this.message = "";
+    this.messageCopy = "";
     this.links = [];
+  }
+
+  ngOnInit() {
+    this.messageCopy = this.message;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["message"] && !changes["message"].firstChange) {
+      this.messageCopy = changes["message"].currentValue;
+    }
   }
 }
