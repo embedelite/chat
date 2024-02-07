aa06b4174a1e040168d18fd36589991c54170418
Lukas Schmyrczyk <lukas.schmyrczyk@lionmint.com>
2024-02-07 21:26:54 +0100

    Enable monthly and yearly groups for chats in sidebar

--- src/app/sidebar.component.ts
+++ src/app/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 import { Chat, ChatService } from "./services/chat.service";
 import { KeyboardShortcutService } from "./services/keyboardshortcut.service";
 
